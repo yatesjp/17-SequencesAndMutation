de924db5c810087e2c71473ab0fdd01615a9901e
yatesjp <yatesjp@rose-hulman.edu>
2018-04-19 10:39:06 -0400

    mldklkdjdwppiccp

--- src/m7_more_mutation.py
+++ src/m7_more_mutation.py
@@ -5,7 +5,7 @@ by changing the values of instance variables.
 Authors: David Mutchler, Amanda Stouder, Chandan Rupakheti, Katie Dion,
          Claude Anderson, Delvin Defoe, Curt Clifton, their colleagues,
          and PUT_YOUR_NAME_HERE.
-"""  # TODO: 1. PUT YOUR NAME IN THE ABOVE LINE.
+"""  # Done: 1. PUT YOUR NAME IN THE ABOVE LINE.
 
 import rosegraphics as rg
 
@@ -169,12 +169,16 @@ def RETURN_circle(circle, color):
     where the color is either a string that Rosegraphics understands
     or a rg.Color object.
     """
-    # TODO: 2. First, READ THE ABOVE TEST CODE.
+    # Done: 2. First, READ THE ABOVE TEST CODE.
     #          Make sure that you understand it.
     #          Then, IMPLEMENT and test THIS FUNCTION
     #          (using the above code for testing).
     #
     # HINT: Use the   clone   method to make the copy.
+
+    ted = circle.clone()
+    ted.fill_color = color
+    return ted
 
 
 def run_test_MUTATE_circle():
@@ -301,11 +305,15 @@ def MUTATE_circle(circle, color, n):
     Precondition: The first argument is a rg.Circle and the second
                   argument is a color appropriate for Zellegraphics.
     """
-    # TODO: 3. First, READ THE ABOVE TEST CODE.
+    # Done: 3. First, READ THE ABOVE TEST CODE.
     #          Make sure that you understand it.
     #          Then, IMPLEMENT and test THIS FUNCTION
     #          (using the above code for testing).
     # HINT: This function can be implemented with just TWO lines of code.
+
+    circle.fill_color = color
+    cent = circle.center
+    circle.center = rg.Point(cent.x + n, cent.y)
 
 
 # ----------------------------------------------------------------------
